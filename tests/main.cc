71b01f69b572685c3594a02fcbbf905c47510c82
donghancheng85 <donghancheng85@gmail.com>
2023-06-16 10:06:42 -0400

    update test main, include gtest_main and remove self written main

--- tests/main.cc
+++ tests/main.cc
@@ -7,9 +7,3 @@ TEST(TestSuiteSample, TestSample)
   int s = sum(2, 4);
   ASSERT_EQ(6, s);
 }
-
-int main(int argc, char **argv)
-{
- testing::InitGoogleTest(&argc, argv);
- return RUN_ALL_TESTS();
-}
